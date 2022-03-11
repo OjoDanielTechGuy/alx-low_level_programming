911b56c8ba5ee767af667d4402d64113b1049805
Godwisdom <danielbankole33@yahoo.com>
2022-03-11 17:34:39 +0100

    program to print last digit after diving by 10

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int L;
+	char fwords[10] = "Last digit of";
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	L = n % 10;
+	
+	if (L > 5)
+	{
+		printf("%s %d is %d and is greater than 5\n", fwords, n, L);   
+	}
+	else if (L == 0)
+	{
+		printf("%s %d is %d and is 0\n", fwords, n, L);
+	}
+	else if (L < 6)
+	{
+		printf("%s, %s is %d and is less than 6 and not 0\n" str, n, fwords);
+	}
+
+	return (0);
+}
