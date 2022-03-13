ec9be6ff7cc245a13b2eaae72740d173ece837ac
Godwisdom <danielbankole33@yahoo.com>
2022-03-13 16:18:41 +0100

    program that prints the lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * desc: program that prints the lowercase alphabet in reverse using only putchar function
+ * You can only use putchar twice
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchat(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
