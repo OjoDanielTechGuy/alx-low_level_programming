58a8107b6fa63080d5d2c323c4adf4975aab9d16
Godwisdom <danielbankole33@yahoo.com>
2022-03-13 16:39:44 +0100

    program that prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * desc: program that prints all possible combinations -
+ * of single-digit numbers
+ * Numbers must be separated by , followed by a space
+ * Numbers should be printed in ascending order using only putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	putchat('\n');
+
+	return (0);
+}
