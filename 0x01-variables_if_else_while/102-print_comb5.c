fa3e5c93b7aaf036f4281aaed1889066b2b14c7f
Godwisdom <danielbankole33@yahoo.com>
2022-03-15 10:00:36 +0100

    added some syntax more lines

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,20 +19,26 @@ int main(void)
 	for (i = 0; i < 100; i++)
 	{
 		a = i / 10;
-		b = j % 10;
+		b = i % 10;
 
-		if (a < c || (a == c && b < d))
+		for (j = 0; j < 100; j++)
 		{
-			putchar(a + '0');
-			putchar(b + '0');
-			putchar(32);
-			putchar(c + '0');
-			putchar(d + '0');
+			c = j / 10;
+			d = j % 10;
 
-			if (!(a == 9 && b == 8))
+			if (a < c || (a == c && b < d))
 			{
-				putchar(44);
+				putchar(a + '0');
+				putchar(b + '0');
 				putchar(32);
+				putchar(c + '0');
+				putchar(d + '0');
+
+				if (!(a == 9 && b == 8))
+				{
+					putchar(44);
+					putchar(32);
+				}
 			}
 		}
 	}
