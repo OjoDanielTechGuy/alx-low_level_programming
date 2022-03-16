2760f20a14cde71337372544835dfa72a8996f3d
Daniel <danielbankole33@yahoo.com>
2022-03-16 11:13:14 +0100

    added description for fn print_sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _print_sign - function that prints the sign of a number.
+ * print_sign - function that prints the sign of a number.
  * @n: is a single alphabet that hold a number
  * desc: Returns 1 and prints + if n is greater than zero
  * Returns 0 and prints 0 if n is zero
