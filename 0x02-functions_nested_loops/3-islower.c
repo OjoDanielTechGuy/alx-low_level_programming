5e7a2aa6b438676e596a3c699c899c8296c6637a
Daniel <danielbankole33@yahoo.com>
2022-03-16 04:36:00 +0100

    added description for parameter c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
  * _islower - function that checks for lowercase character.
  * desc: Returns 1 if c is lowercase
  * Returns 0 otherwise
- * Return: Always 0 (Success)
+ * Return: 1 (Success)
  */
 int _islower(int c)
 {
