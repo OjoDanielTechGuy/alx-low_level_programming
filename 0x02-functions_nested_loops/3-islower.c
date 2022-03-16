f28c78a634a546e8583f4c594ffcba55befbd48f
Daniel <danielbankole33@yahoo.com>
2022-03-16 04:41:52 +0100

    added description for return value of fn _islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,6 +3,7 @@
  * _islower - function that checks for lowercase character.
  * @c: is a single alphabet that is inserted to check the function
  * desc: Returns 1 if c is lowercase, else 0
+ * Return: 1 (Success)
  */
 int _islower(int c)
 {
