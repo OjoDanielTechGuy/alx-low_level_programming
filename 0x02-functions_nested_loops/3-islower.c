182ac2d0ed6da2dd36eabbaeab013edc325edbc5
Daniel <danielbankole33@yahoo.com>
2022-03-16 04:23:47 +0100

    command that checks for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * is_lower - function that checks for lowercase character.
+ * desc: Returns 1 if c is lowercase
+ * Returns 0 otherwise
+ * Return: Always 0 (Success)
+ */
+int is_lower(int c)
+{
+	char ch;
+
+	if (ch >= 'a' && ch <= 'z')
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+
+}
