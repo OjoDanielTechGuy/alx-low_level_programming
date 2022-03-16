8497153cb384824169fef44ac9437ef206c43592
Daniel <danielbankole33@yahoo.com>
2022-03-16 14:25:41 +0100

    description for text x and y in betty documentation

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * @x - receives first number
- * @y - receives second number
- * desc:function that adds two integers and returns the result
+ * add - function that adds two integers and returns the result
+ * @x: receives first number
+ * @y: receives second number
  * Return: addition of two integers
  */
 int add(int x, int y)
