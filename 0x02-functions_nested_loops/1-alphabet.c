18da74a940281307f0bfcef40ca63bd07e565d3c
Daniel <danielbankole33@yahoo.com>
2022-03-16 03:41:57 +0100

    put main.h as a file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 
 /**
  * main - Entry Point
@@ -18,5 +18,5 @@ void print_alphabet(void)
 	}
 	_putchar('\n');
 
-	return (0)
+	return (0);
 }
