4b65d85eb695f24a5aa2681705db1f5e735f7526
Daniel <danielbankole33@yahoo.com>
2022-03-17 13:46:32 +0100

    reduced line in a function

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,8 +3,8 @@
  * print_times_table - printing the n times table from 0
  * @n: number of the times table given
  * desc: x is first iteration
- * y is second iteration
- * z is used as a holding value variable
+ * y is second iteratio
+ * z is a answer variable holder
  * Return: times table of number n
  */
 void print_times_table(int n)
@@ -18,28 +18,24 @@ void print_times_table(int n)
 			for (y = 0; y <= n; y++)
 			{
 				z = y * x;
-
 				if (y == 0)
 				{
 					_putchar(z + '0');
-				}
-				else if (z < 10 && y != 0)
+				}else if (z < 10 && y != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(z + '0');
-				}
-				else if (z >= 10 && z < 100)
+				}else if (z >= 10 && z < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((z / 10) + '0');
 					_putchar((z % 10) + '0');
-				}
-				else if (z >= 100)
+				}else if (z >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
