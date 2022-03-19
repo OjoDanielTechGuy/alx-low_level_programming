d0b833638d27a41dca93d9c7c9fe315f9568c992
Daniel <danielbankole33@yahoo.com>
2022-03-19 16:16:06 +0100

    fixed betty documentation style

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,6 @@
 /**
  * main - program that prints
  * the numbers from 1 to 100
- * @size: represents integer number
  * desc: for multiples of three print Fizz
  * instead of the number and for the multiples
  * of five print Buzz
@@ -22,15 +21,25 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i == 100)
+		{
 			printf("%s", b);
+		}
 		else if ((i % 3 == 0) && (i % 5 == 0))
+		{
 			printf("%s ", fb);
+		}
 		else if (i % 3 == 0)
+		{
 			printf("%s ", f);
+		}
 		else if (i % 5 == 0)
+		{
 			printf("%s ", b);
+		}
 		else
+		{
 			printf("%d ", i);
+		}
 	}
 	printf("\n");
 
