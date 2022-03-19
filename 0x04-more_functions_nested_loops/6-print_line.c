7394537f9ab7cdc74f8ee514c6ddc421bee5969e
Daniel <danielbankole33@yahoo.com>
2022-03-19 15:31:16 +0100

    functions that prints a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_line - function that draws a straight line
- * in the terminal.
+ * in terminal
  * desc: only use _putchar function to print
  * @n: Where n is the number of times the character
  * _ should be printed
