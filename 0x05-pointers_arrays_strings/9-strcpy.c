e3d3e7cfc46034ac24b0c35de4aeafd6a6cbdf9a
Daniel <danielbankole33@yahoo.com>
2022-03-22 10:35:11 +0100

    functions that copy to another variable using pointers

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcpy - Entry Point
+ * des: function that copies the string pointed to by src
+ *  including the terminating null byte (\0),
+ *  to the buffer pointed to by dest.
+ *  @dest: copy to buffer
+ *  @src: copy from pointer src
+ *  Return: String Value
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int x = 0;
+
+	for (; src[x] != '\0'; i++)
+	{
+		dest[x] = src[x];
+	}
+	
+	return (dest);
+}
