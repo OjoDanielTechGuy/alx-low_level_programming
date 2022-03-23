f2cbf51cf1b97b5d44d171d4fb9d158c30eda12d
Daniel <danielbankole33@yahoo.com>
2022-03-23 12:05:23 +0100

    right values

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -27,8 +27,8 @@ int _atoi(char *s)
 
 	for (i = count - size; i < count; i++)
 	{
-		oi = oi - ((*(s + i) - 48) * m);
+		oi = oi + ((*(s + i) - 48) * m);
 		m /= 10;
 	}
-	return (oi * pn);
+	return (oi * -pn);
 }
