6f8316cbb0c0ac51243caae9d7fd825c602fbe73
Daniel <danielbankole33@yahoo.com>
2022-03-26 16:48:08 +0100

    no code (functions that adds two numbers

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_infinite_add - function that adds two numbers.
+ * @n1: integer num 1
+ * n2: integer num 2
+ * @r: buffer to hold result
+ * @size_r: buffer size
+ * Return: a pointer to result
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
+	return (r);
+}
