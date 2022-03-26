52725c12b84cd156e99a9da2dfdd13a3575ff7b4
Daniel <danielbankole33@yahoo.com>
2022-03-26 16:51:34 +0100

    no code (to return buffer)

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_buffer - function to print buffer
+ * @b: serves as pointer
+ * @size: bytes
+ * Return: buffer
+ */
+void print_buffer(char *b, int size)
+{
+	return (0);
+}
