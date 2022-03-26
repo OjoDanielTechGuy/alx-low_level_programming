743f09c9a4a11660f91b7bff918f2a8ccdaf45a5
Daniel <danielbankole33@yahoo.com>
2022-03-26 14:26:35 +0100

    removed undeclared variable

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,13 +7,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int x = 0;
-	int y = 0;
-	
-	while (s1[x] != '\0' && y == 0)
-	{
-		b = s1[x] - s2[x];
-		x++;
-	}
-	return (y);
+    int x = 0;
+    int y = 0;
+
+    while (s1[x] != '\0' && y == 0)
+    {
+        y = s1[x] - s2[x];
+        x++;
+    }
+    return (y);
 }
