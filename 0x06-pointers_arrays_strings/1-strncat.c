0abb56519f1b3a115affe75596d300159133da1b
Daniel <danielbankole33@yahoo.com>
2022-03-26 14:08:25 +0100

    fixing betty style

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,12 +2,11 @@
 /**
  * _strncat - function that concatenates two strings
  * desc: The _strncat function is similar to the _strcat function,
- * except that it will use at most n bytes from src; and
- * src does not need to be null-terminated if it contains
- * n or more bytes
+ * except that it will use at most n bytes from src and
+ * src does not need to be null-terminated
  * @dest: string
  * @src: number of bytes
- * @n: rep. bytes
+ * @n: represent iterations
  * Return: a pointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
