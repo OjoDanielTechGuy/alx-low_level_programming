1c96be7d4f7e38a9c99033f28599eceef279a4cb
Daniel <danielbankole33@yahoo.com>
2022-04-05 20:18:40 +0100

    program that prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints number of arguement
+ * @argc: counts the arguement
+ * @argv: array that leads to an arguement
+ * Return: number of arguements
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
