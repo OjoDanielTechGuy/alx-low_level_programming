c46a38e7b8f950d39dd1324a48dcaf1f9a392313
Daniel <danielbankole33@yahoo.com>
2022-04-05 16:51:02 +0100

    function that splits a string into words1

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stlib.h>
+#include <stdio.h>
+/**
+ * strtow - function that splits a string into words
+ * @str: accepts string
+ * desc:The function returns a pointer to an array of strings
+ * Each element of this array should contain a single word, null-terminated
+ * The last element of the returned array should be NULL
+ * Words are separated by spaces
+ * Return: NULL if str == NULL or str == ""
+ * If your function fails, it should return NULL
+ */
+char **strtow(char *str)
+{
+	Return (0);
+}
