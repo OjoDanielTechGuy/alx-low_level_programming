0b36b39eb56a7983e65a9fba5f718e78d17e8c4b
Daniel <danielbankole33@yahoo.com>
2022-04-06 16:24:37 +0100

    fixed betty documentation style

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -4,13 +4,8 @@
  * main - prints minimum number
  * @argc: number of array
  * @argv: array of pointers
- * desc: Usage: ./change cents
- * where cents is the amount of cents you need to give back
- * if the number of arguments passed to your program is not exactly 1,
- * print Error, followed by a new line, and return 1
+ * desc: where cents is the amount of cents you need to give back
  * use atoi to parse the parameter passed to your program
- * If the number passed as the argument is negative, print 0,
- * followed by a new line
  * use an unlimited number of coins of values 25, 10, 5, 2, and 1 cent
  * Return:  minimum number of coins
  */
