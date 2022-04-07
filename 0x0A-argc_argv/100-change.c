ae332191faedfdbf6b5eb5057389f4c8f9b66238
Daniel <danielbankole33@yahoo.com>
2022-04-07 12:05:53 +0100

    fixed betty documentation style3

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -5,8 +5,6 @@
  * @argc: number of array
  * @argv: array of pointers
  * desc: where cents is the amount of cents you need to give back
- * use atoi to parse the parameter passed to your program
- * use an unlimited number of coins of values 25, 10, 5, 2, and 1 cent
  * Return:  minimum number of coins
  */
 int main(int argc, char *argv[])
@@ -20,7 +18,6 @@ int main(int argc, char *argv[])
 
 		return (1);
 	}
-
 	cents = atoi(argv[1]);
 
 	while (cents > 0)
@@ -51,8 +48,6 @@ int main(int argc, char *argv[])
 			coins++;
 		}
 	}
-
 	printf("%d\n", coins);
-
 	return (0);
 }
