fe2197ba54e7cb4d28c49848f1878c0be6dc253a
Daniel <danielbankole33@yahoo.com>
2022-04-14 17:47:17 +0100

    reconfigured syntax to get the right output

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,28 +11,23 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list strings;
 	char *str;
-	unsigned int i;
-
-	if (separator == NULL)
-	{
-		return;
-	}
+	unsigned int index;
 
 	va_start(strings, n);
 
-	for (i = 0; i < n; i++)
+	for (index = 0; index < n; index++)
 	{
 		str = va_arg(strings, char *);
+
 		if (str == NULL)
-		{
-			str = "(nil)";
-		}
-		printf("%s", str);
-		if (i != (n - 1))
-		{
+			printf("(nil)");
+		else
+			printf("%s", str);
+
+		if (index != (n - 1) && separator != NULL)
 			printf("%s", separator);
-		}
 	}
+
 	printf("\n");
 
 	va_end(strings);
